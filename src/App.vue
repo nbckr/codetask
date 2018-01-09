27f11cc133ee27613c72066ef754320efe8f1b68
Niels Boecker <niels.boecker@maibornwolff.de>
2018-01-09 18:04:12 +0100

    Create a nice background with vue-particle

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <template>
+  <!--<router-view></router-view>-->
+
   <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <router-view></router-view>
-    </main>
+    <!--<img src="./assets/logo.png">-->
+    <div class="wrap-banner">
+      <div class="main-title">
+        <h2>CodeTask</h2>
+        <p class="subtitle">
+          Learn Scala with ease
+        </p>
+        <br>
+        <button>Start Learning Now</button>
+      </div>
+      <vue-particles
+        color="#ffffff"
+        :particleOpacity="0.7"
+        linesColor="#ffffff"
+        :particlesNumber="80"
+        shapeType="circle"
+        :particleSize="5"
+        :linesWidth="2"
+        :lineLinked="true"
+        :lineOpacity="0.4"
+        :linesDistance="150"
+        :moveSpeed="3"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+      >
+      </vue-particles>
+
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -17,15 +43,19 @@ export default {
 </script>
 
 <style>
-body {
+body, html {
   margin: 0;
+  height: 100%;
+  background: #34393f;
 }
 
 #app {
+  height: 100%;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  text-align: center;
 }
 
 main {
@@ -50,5 +80,43 @@ header span {
   font-weight: 400;
   box-sizing: border-box;
   padding-top: 16px;
+}
+
+#particles-js {
+  background-image: url("https://ak9.picdn.net/shutterstock/videos/33473869/thumb/1.jpg");
+  background-size: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.wrap-banner {
+  position: relative;
+  -webkit-align-items: center;
+  -ms-flex-align: center;
+  align-items: center;
+  display: -webkit-flex;
+  display: flex;
+  height: 100%;
+  justify-content: center;
+}
+.main-title {
+  color: #555;
+  z-index: 999;
+}
+
+.main-title h2 {
+  font-size: 4rem;
+  font-family: 'Teko', sans-serif;
+  text-shadow: 3px 3px #777;
+  text-transform: uppercase;
+  margin: 0;
+}
+
+.main-title p.subtitle {
+  font-size: 1.7rem;
+  margin: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueParticles from 'vue-particles'
 
 Vue.config.productionTip = false
+
+Vue.use(VueParticles)
 
 /* eslint-disable no-new */
 new Vue({
