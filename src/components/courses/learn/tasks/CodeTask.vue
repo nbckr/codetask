24969d32115333dd538386fe7dc635ef71ebc540
Niels Boecker <niels.boecker@maibornwolff.de>
2018-01-13 01:52:04 +0100

    Add CodeTask

--- src/components/courses/learn/tasks/CodeTask.vue
+++ src/components/courses/learn/tasks/CodeTask.vue
@@ -1,10 +1,53 @@
 <template>
+
+  <codemirror
+    :value="activeTask.data.code"
+    :options="codemirrorOptions"
+  />
+
 </template>
 
 <script>
-export default {
-};
+  import { codemirror } from 'vue-codemirror';
+  import 'codemirror/lib/codemirror.css';
+  import 'codemirror/theme/monokai.css';    // Theme
+  import 'codemirror/mode/clike/clike';     // Scala
+
+  export default {
+    data () {
+      return {
+        codemirrorOptions: {
+          tabSize: 4,
+          styleActiveLine: true,
+          lineNumbers: true,
+          mode: 'text/x-scala',
+          theme: 'monokai'
+        }
+      };
+    },
+
+    props: ['activeTask'],
+
+    components: {
+      codemirror
+    }
+  }
+  ;
 </script>
 
-<style scoped>
+<style>
+  /* TODO: Duplicated code */
+
+  .CodeMirror {
+    padding: 1rem;
+    line-height: 1.2rem !important;
+  }
+
+  .CodeMirror-linenumbers {
+    width: 1.5rem;
+  }
+
+  .CodeMirror-linenumber {
+    text-align: center;
+  }
 </style>
--- src/components/courses/learn/tasks/Task.vue
+++ src/components/courses/learn/tasks/Task.vue
@@ -4,7 +4,7 @@
     <section>
 
       <div class="description">
-        {{ activeTask.data.description }}
+        <p>{{ activeTask.data.description.trim() }}</p>
       </div>
 
       <app-koan-task
@@ -16,6 +16,12 @@
         v-if="activeTask.tag === 'video-task'"
         :active-task="activeTask"
       />
+
+      <app-code-task
+        v-if="activeTask.tag === 'code-task'"
+        :active-task="activeTask"
+      />
+
 
       <br>
       <el-button
@@ -32,11 +38,13 @@
 
 <script>
   import KoanTask from './KoanTask';
+  import CodeTask from './CodeTask';
   import VideoTask from './VideoTask';
 
   export default {
     components: {
       appKoanTask: KoanTask,
+      appCodeTask: CodeTask,
       appVideoTask: VideoTask
     },
 
@@ -62,16 +70,19 @@
     background-color: white;
     display: inline-block;
     padding: 1.6rem;
-    /* white-space: pre; */
   }
 
-  #halloX {
-    font-family: monospace;
-    color: #D4D4BD;
-    background-color: #1E1E1E;
+  .description {
+    border: lightgrey 1px solid;
+    border-radius: 0.3rem;
+    /* box-shadow: darkgrey 3px 3px; */
 
-    background-color: green;
-    max-width: available;
+    padding: 0.8rem;
+    margin-bottom: 1.5rem;
+  }
+  .description > p {
+    margin: 0;
+    /* white-space: pre; */
   }
 
 </style>
